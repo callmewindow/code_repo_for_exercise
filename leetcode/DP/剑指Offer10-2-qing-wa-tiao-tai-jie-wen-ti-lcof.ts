3f4c12ca38594972cf66e114b0cd1de6d57f23ac
xuanw <847590417@qq.com>
2022-11-10 01:18:52 +0800

    新增部分动态规划和链表题

---
+++ leetcode/DP/剑指Offer10-2-qing-wa-tiao-tai-jie-wen-ti-lcof.ts
@@ -0,0 +1,16 @@
+// https://leetcode.cn/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
+// 直接求解即可，两个dp
+function numWays(n: number): number {
+    const mod = 1000000007n;
+    // 到n的跳法可以是从n-1跳一下上来，也可以n-2跳两下上来
+    if(n == 0) return 1;
+    if(n == 1) return 1;
+    // 三个变量实现处理
+    let num0 = 1n, num1 = 1n, num2 = 0n;
+    for(let i = 2;i<=n;i++){
+        num2 = num0 + num1;
+        num0 = num1;
+        num1 = num2;
+    }
+    return Number(num2 % mod);
+};
---
+++ leetcode/list&tree/剑指Offer28-dui-cheng-de-er-cha-shu-lcof.ts
@@ -0,0 +1,38 @@
+// https://leetcode.cn/problems/dui-cheng-de-er-cha-shu-lcof
+// dfs可以更加准确的判断是否相同，可以规避同层影响
+function isSymmetric(root: TreeNode | null): boolean {
+    // 对左节点做前序遍历，对右节点做后序遍历，看结构是否一致，但对空节点需要填补，防止都是左叶子情况的出现
+    // bfs无法处理同一层数据平移的问题，需要使用dfs
+    if(root == null) return true;
+    let leftN = root.left, rightN = root.right;
+    // 要么全空要么全有
+    if(leftN == null && rightN == null) return true;
+    if(leftN == null || rightN == null) return false;
+    let leftQ = [], rightQ = [];
+    leftQ.push(leftN);
+    rightQ.push(rightN);
+    let tmpN: TreeNode;
+    while(leftQ.length > 0 && rightQ.length > 0){
+        // 同步处理左右，判断值是否相等
+        tmpN = leftQ.shift();
+        const lVal = tmpN == null ? null : tmpN.val;
+        // 只有当自己空或是叶子结点才不传值
+        if(tmpN != null && (tmpN.left != null || tmpN.right != null)){
+            // 否则无论是否空都需要传入，以完全判断
+            leftQ.unshift(tmpN.left);
+            leftQ.unshift(tmpN.right);
+        }
+        // 右边后序遍历，需要调整
+        tmpN = rightQ.pop()
+        const rVal = tmpN == null ? null : tmpN.val;
+        if(tmpN != null && (tmpN.left != null || tmpN.right != null)){
+            rightQ.push(tmpN.right);
+            rightQ.push(tmpN.left);
+        }
+        // console.log(lVal, rVal, lVal == rVal, lVal != rVal);
+        if(lVal != rVal) return false;
+
+    }
+    // 也有可能因为长度不一致提前退出，需判断是否都是长度0
+    return leftQ.length == 0 && rightQ.length == 0;
+};
