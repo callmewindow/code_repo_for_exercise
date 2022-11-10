ba59d6c06e6d7cb8de2876b338940557b9bc4f76
xuanw <847590417@qq.com>
2022-11-10 23:07:00 +0800

    新增遗漏题目，调整部分文件为2空格锁进格式

--- leetcode/DP/剑指Offer42-lian-xu-zi-shu-zu-de-zui-da-he-lcof.ts
+++ leetcode/DP/剑指Offer42-lian-xu-zi-shu-zu-de-zui-da-he-lcof.ts
@@ -1,29 +1,29 @@
 // https://leetcode.cn/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof
 // 两个位置分别保存当前值是否记录来实现动态
 function maxSubArray(nums: number[]): number {
-    // 连续的数组，因此在每个位置记录该位置开始，保存后面若干个元素的连续子数组最大和
-    // 用两个值分别保存记录当前值和不记录当前值的情况
-    // 这样dp[0]判断记录或不记录哪个最大就是就是最大的连续子数组和
-    const nLen = nums.length
-    let maxSubSum = Array(nLen)
-      .fill(0)
-      .map(
-        () => Array(2).fill(Number.MIN_SAFE_INTEGER)
-      );
-  
-    // 初始化边界值，记录就是自身+后面的所有，不记录就是最小值
-    maxSubSum[nLen - 1][0] = nums[nLen - 1];
-    for(let i = nLen - 2;i>=0;i--)
-      maxSubSum[i][0] = nums[i] + maxSubSum[i+1][0];
+  // 连续的数组，因此在每个位置记录该位置开始，保存后面若干个元素的连续子数组最大和
+  // 用两个值分别保存记录当前值和不记录当前值的情况
+  // 这样dp[0]判断记录或不记录哪个最大就是就是最大的连续子数组和
+  const nLen = nums.length
+  let maxSubSum = Array(nLen)
+    .fill(0)
+    .map(
+      () => Array(2).fill(Number.MIN_SAFE_INTEGER)
+    );
+
+  // 初始化边界值，记录就是自身+后面的所有，不记录就是最小值
+  maxSubSum[nLen - 1][0] = nums[nLen - 1];
+  for (let i = nLen - 2; i >= 0; i--)
+    maxSubSum[i][0] = nums[i] + maxSubSum[i + 1][0];
+  // console.log(maxSubSum);
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    // 如果记录当前值，则该值就等于后面记录了的值和自身进行比较
+    maxSubSum[i][0] = Math.max(nums[i], nums[i] + maxSubSum[i + 1][0]);
+    // 如果不记录当前值，最大值就等于后一个数记录或不记录的最大值
+    maxSubSum[i][1] = Math.max(maxSubSum[i + 1][0], maxSubSum[i + 1][1]);
     // console.log(maxSubSum);
-  
-    for(let i = nums.length - 2;i>=0;i--){
-      // 如果记录当前值，则该值就等于后面记录了的值和自身进行比较
-      maxSubSum[i][0] = Math.max(nums[i], nums[i] + maxSubSum[i+1][0]);
-      // 如果不记录当前值，最大值就等于后一个数记录或不记录的最大值
-      maxSubSum[i][1] = Math.max(maxSubSum[i+1][0], maxSubSum[i+1][1]);
-      // console.log(maxSubSum);
-    }
-    
-    return Math.max(...maxSubSum[0])
-  };
+  }
+
+  return Math.max(...maxSubSum[0])
+};
---
+++ leetcode/DP/剑指Offer46-ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof.ts
@@ -0,0 +1,43 @@
+// https://leetcode.cn/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof
+// 分别记录和不合并的情况做处理
+function checkMerge(ch1: string, ch2: string): boolean {
+  return ch1 == '1' || (ch1 == '2' && Number(ch2) <= 5)
+}
+function translateNum(num: number): number {
+  // 可能有多种情况的有：1x，2x，可以拆分为1和x，进而再递归
+  // 需要倒序处理
+  // 这里先转成字符串数组，其实可以不转直接弄，应该更快
+  let numA = num.toString().split('');
+  let nLen = numA.length;
+  if (nLen == 1) return 1;
+  if (nLen == 2) {
+    // 判断能否和后一个数合并
+    if (checkMerge(numA[nLen - 2], numA[nLen - 1])) return 2;
+    return 1;
+  }
+
+  // 两个状态，一个是自己当作一个数处理，一个是自己和后面的一个数绑定起来进行可能性的计算
+  let transNum = Array(nLen).fill(0)
+    .map(
+      () => Array(2).fill(0)
+    );
+  // 初始化边界值
+  // 最右一个数肯定只有一种情况
+  transNum[nLen - 1] = [1, 0];
+  // 倒数第二个数需要根据情况判断
+  transNum[nLen - 2] = checkMerge(numA[nLen - 2], numA[nLen - 1]) ? [1, 1] : [1, 0];
+  // console.log(transNum)
+
+  // 倒数第三个数开始
+  for (let i = nLen - 3; i >= 0; i--) {
+    // 如果自己一个数，等于后一位两种情况之和
+    transNum[i][0] = transNum[i + 1][0] + transNum[i + 1][1]
+    // 如果和后面的能合起来一个数，则等于需要后一个数的后一个数两种情况之和
+    if (checkMerge(numA[i], numA[i + 1]))
+      transNum[i][1] = transNum[i + 2][0] + transNum[i + 2][1];
+    // console.log(transNum)
+  }
+
+  // 两种情况是不同的，直接合并即可
+  return transNum[0][0] + transNum[0][1];
+};
